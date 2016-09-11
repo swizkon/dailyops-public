ef449578a19ca736c9b24d879c955b63054c880a
Jonas Jerndin <jonas.jerndin@ne.se>
2016-09-11 13:09:51 +0200

    Adde more guard clauses for deposits and transfers.

--- src/DailyOps.NET/UnitTestProject2/UnitTest1.cs
+++ src/DailyOps.NET/UnitTestProject2/UnitTest1.cs
@@ -27,6 +27,21 @@ namespace Bank
         }
 
         [Test]
+        public void A_deposit_must_be_a_positive_ammount()
+        {
+            // Arrange
+            Random rnd = new Random();
+            Account source = new Account();
+
+            var exp = Assert.Throws<ArgumentException>(() =>
+            {
+                // Act
+                source.Deposit(0m - rnd.Next(0, 100));
+            });
+        }
+
+
+        [Test]
         public void When_transfer_ammount_is_greater_than_balance_throw()
         {
             // Arrange
@@ -41,6 +56,24 @@ namespace Bank
                 source.TransferFunds(destination, 100m);
             });
         }
+
+        [Test]
+        public void When_transfer_to_a_null_account_throw()
+        {
+            // Arrange
+            Account source = new Account();
+            source.Deposit(1000m);
+
+            // Act
+            Account destination = null;
+
+            Assert.Throws<ArgumentNullException>(() =>
+            {
+                // Act
+                source.TransferFunds(destination, 100m);
+            });
+        }
+
     }
 
     internal class Account
@@ -49,11 +82,16 @@ namespace Bank
 
         internal void Deposit(decimal v)
         {
+            if (v <= 0)
+                throw new ArgumentException(nameof(v));
             Balance += v;
         }
 
         internal void TransferFunds(Account destination, decimal v)
         {
+            if (destination == null)
+                throw new ArgumentNullException(nameof(destination));
+
             if (Balance < v)
                 throw new InvalidOperationException("The Balance must be greater that the amount to deposit");
 
